3f79c93ee5595d14875761322613684360e3b9ab
heshamshahtout <heshamshahtout@gmail.com>
2017-11-17 21:44:26 +0200

    Final2

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
-<<<<<<< HEAD
 #include <signal.h>
 
 int launch(char **args,int background);
@@ -16,14 +15,9 @@ int changeDirectory(char* args[]);
 int flag =0 ;
 int x = 0;
 int main()
-=======
-void launch(char **args,int background);
-    int no_reprint_prmpt = 0;
-void remove_EOL(char line[])
->>>>>>> 51eccf5adce365df3f89a2c796e73d2924aa55d6
 {
-    char** args; 
-    args = (char *) malloc(10*(sizeof(char**)));
+    char** args;
+    args = (char *) malloc(10*(sizeof(char**))); 
     char line[100];
     while(1)
     {
@@ -33,19 +27,19 @@ void remove_EOL(char line[])
     return 0;
 }
 
-void shellPrompt()
+void shellPrompt() 
 {
-    char cwd[1024];
+    char cwd[1024]; 
     getcwd(cwd,sizeof(cwd));
 
     char host[1204] = "";
     gethostname(host, sizeof(host));
-    printf("%s@%s:%s> ", getenv("LOGNAME"), host,cwd);
+    printf("%s@%s:%s> ", getenv("LOGNAME"), host,cwd); 
 }
 
 int parse_line(char*args[],char line[])
 {
-    read_line(line);
+    read_line(line);  
     if(flag ==1)
     {
         flag = 0;
@@ -57,18 +51,13 @@ int parse_line(char*args[],char line[])
 
 void read_line(char line[])
 {
-<<<<<<< HEAD
-    char *x= fgets(line,100,stdin);
-=======
-    char* ret = fgets(line,100,stdin);
-
->>>>>>> 51eccf5adce365df3f89a2c796e73d2924aa55d6
+    fgets(line,100,stdin); 
     remove_EOL(line);
 }
 
 void remove_EOL(char line[])
 {
-    int i, length,j=0;
+    int i, length,j=0,count =0;
     int temp[100];
     length = strlen(line);
     for(i = 0 ; i < length ; i++) {
@@ -76,20 +65,24 @@ void remove_EOL(char line[])
             line[i] = '\0' ;
     }
     for (i = 0; i < length; ++i) {
-        temp[j] = line[i];
-        j++;
+        if(line[i] == ' ')
+            count++;
+    }
+    if(count == i-1)
+    {
+        flag = 1;
     }
     char x = line[0];
-    char *p = strtok (temp, " ");
-    if(strcmp(line,"\0")==0||p == NULL)
+    if(strcmp(line,"\0")==0)
     {
         flag =1;
     }
+
 }
 
 int split_line(char* args[],char line[])
 {
-    char ** result  = NULL;
+    char ** result  = NULL; 
     char *  p    = strtok (line, " ");
     int n = 0, i=0,j=0,background=0,z=0;
     while (p) {
@@ -99,8 +92,8 @@ int split_line(char* args[],char line[])
         result[n-1] = p;
         p = strtok (NULL, " ");
     }
-    result = realloc (result, sizeof (char*) * (n+1));
-    result[n] = 0;
+    //result = realloc (result, sizeof (char*) * (n+1));
+    //result[n] = 0;
     for (i = 0; i < (n+1); ++i) {
         args[j] = result[i];
         j++;
@@ -135,15 +128,10 @@ int launch(char **args,int background)
 		 return 0;
 	 }
      if(pid==0){
-<<<<<<< HEAD
          x = execvp(args[0],args);
          if (x ==-1){
 			printf("Command %s not found\n",args[0]);
            exit(0);
-=======
-         if (execvp(args[0],args)==err){
-			printf("Command %s not found\n",args[0]);
->>>>>>> 51eccf5adce365df3f89a2c796e73d2924aa55d6
 		}
      }
      if(background == 0)
@@ -156,25 +144,6 @@ int launch(char **args,int background)
      }
      return 1;
 }
-<<<<<<< HEAD
-=======
-int parse_line(char*args[],char line[])
-{
-	if(line == NULL)
-	{
-		free(line);
-		return 0;
-	}
-    read_line(line);
-    XXXXX(args,line);
-    return 1;
-}
-void shellPrompt()
-{
-    // We print the prompt in the form "<user>@<host> <cwd> >"
-    char cwd[1024];
-    getcwd(cwd,sizeof(cwd));
->>>>>>> 51eccf5adce365df3f89a2c796e73d2924aa55d6
 
 int changeDirectory(char* args[])
 {
@@ -198,24 +167,4 @@ int changeDirectory(char* args[])
         }
     }
     return 0;
-<<<<<<< HEAD
 }
-=======
-}
-int main()
-{
-    char** args;
-    args = (char *) malloc(10*(sizeof(char**)));
-    char line[100];
-    pid_t pid, wpid;
-    int status; 
-
-    int flag = 1;
-    while(flag!=0)
-    {
-        shellPrompt();
-        parse_line(args,line);
-    }
-    return 0;
-}
->>>>>>> 51eccf5adce365df3f89a2c796e73d2924aa55d6
